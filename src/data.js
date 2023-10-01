00cdce8a417ca8283e030956045f770bd10eb2c3
Russ Perry <rperry1886@gmail.com>
2023-10-01 13:43:01 -0400

    Add: data.js

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    imageURL: 'hocking-hills.jpg',
+    location: 'Ohio',
+    mapURL: '#',
+    destination: 'Hocking Hills',
+    travelDate: 'Sep 22nd, 2023 - Sep 24th, 2023',
+    description:
+      "We went to the Hocking Hills Adventure campgound and camped in tents for 3 days (2 nights). We hiked at Old Man's Cave, went to a moonshine distillery, and walked around the town of Logan. Apparently, Bigfoot is a huge deal in Logan. Every store had a lot fo different memorobelia for him.",
+  },
+  {
+    imageURL: 'key-west.jpg',
+    location: 'Florida',
+    mapURL: '#',
+    destination: 'Key West',
+    travelDate: 'Jul 22nd, 2021 - Jul 25th, 2021',
+    description:
+      'My friend and I took a spontaneous trip to Key West, Florida. This was the first time I was ever on an airplane. Flying is so much more convenient than driving. We did an excustion where we took a sail boat out, kayaked, and snorkeled. We also walked around Duval Street. We had the best lobster rolls I ever had when we were down there as well.',
+  },
+  {
+    imageURL: 'too-many-games.jpg',
+    location: 'Pennsylvania',
+    mapURL: '#',
+    destination: 'Too Many Games',
+    travelDate: 'Jun 24th, 2021 - Jun 27th, 2021',
+    description:
+      'I made my way down to Oaks PA for the video game convention Too Many Games, hosted by James Rolfe, or better known as Angry Video Game Nerd. This is a huge convention for fans of video games, both new and old. I got to meet some famous people in the video game world, and walk around the huge vendor room, trying not to spend every dime I have on video games.',
+  },
+];
